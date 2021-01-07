677d8f66682199dac5ea4dc034b46f046ad78891
kinoko-2 <apymajl2sv8npjae@icloud.com>
2021-01-08 00:36:53 +0900

    空白空行　変数名　条件分岐修正

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -1,42 +1,46 @@
 #!/usr/bin/env ruby
 
 require "date"
-require 'optparse'
-
-def calender (y: Date.today.year,m: Date.today.mon)
-    
-    week=" 日 月 火 水 木 金 土 "
-    puts "#{m}月　#{y}".center(20)
-    puts week
-
-    month_lastday=Date.new(y,m,-1).day
-    (1..month_lastday).each do |x|
-
-        x_day=Date.new(y,m,x)
-        if x==1
-            print " ".rjust(3)*(x_day.wday)
-            print x.to_s.rjust(3)
-        elsif (x_day.wday)==6
-            print x.to_s.rjust(3)+"\n"
-        else
-            print x.to_s.rjust(3)
-        end
-    
+require "optparse"
+class Calendar
+  def decide_date (year , month)
+    if year != nil 
+      @year = year.to_i
+    else
+      @year = Date.today.year
     end
-    print "\n"
-   
-end 
-
-argv=ARGV.getopts("y:m:")
-
-if argv["y"]!=nil && argv["m"]!=nil
-    calender(y: argv["y"].to_i , m: argv["m"].to_i)
-elsif argv["m"]!=nil 
-    calender(m: argv["m"].to_i)
-elsif argv["y"]!=nil
-    calender(y: argv["y"].to_i )
-else
-    calender
+    if 1 <= month.to_i && month.to_i <= 12 && month != nil
+      @month = month.to_i
+    else
+      @month = Date.today.mon
+    end  
+  end
+  def calendar_display
+    week=" 日 月 火 水 木 金 土 "
+    puts "#{@month}月　#{@year}".center(20)
+    puts week
+    month_lastday = Date.new(@year , @month , -1).day
+    (1..month_lastday).each do |day|
+      current_date=Date.new(@year , @month , day)
+        if day == 1
+          print " ".rjust(3) * (current_date.wday)
+          if current_date.strftime("%a") == "Sat"
+            
+            puts day.to_s.rjust(3)
+          else
+            print day.to_s.rjust(3)
+          end
+        elsif current_date.strftime("%a") == "Sat"
+          puts day.to_s.rjust(3)
+        else
+          print day.to_s.rjust(3)
+        end  
+      end
+      print "\n"  
+  end 
 end
 
-
+argv=ARGV.getopts("y:m:")
+calendar=Calendar.new
+calendar.decide_date(argv["y"] , argv["m"])
+calendar.calendar_display
